14ae64901b9017fb1e3a4e4662753fd89b6cd73a
Mazharul Islam <mazharul430@gmail.com>
2023-03-01 02:58:34 -0800

    added login and register page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h1>This is Login Page</h1>
+        <div className="hero min-h-screen">
+            <div className="hero-content lg:w-[550px] w-full flex-col lg:flex-row-reverse">
+                <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <h1 className='text-2xl font-bold pt-5 uppercase'>Login Now</h1>
+                        <form action="">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <Link className="label-text-alt link link-hover">Forgot password?</Link>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn border-none bg-cyan-700 ">Login</button>
+                            </div>
+                        </form>
+                        <div className="flex flex-col w-full border-opacity-50">
+                            <div className="divider">OR</div>
+                        </div>
+                        <div className="form-control">
+                            <button className="btn border-none bg-cyan-700 ">Login With Google</button>
+                        </div>
+                        <div className="flex p-[1px] justify-between">
+                            <span>Create a new account</span>
+                            <span className='hover:text-red-400'> <Link to='/register'>Please register</Link></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h1>This is Register Page</h1>
+        <div className="hero min-h-screen">
+            <div className="hero-content lg:w-[550px] w-full flex-col lg:flex-row-reverse">
+                <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
+                    <from className="card-body">
+                        <h1 className='text-2xl font-bold pt-5 uppercase'>Register Now</h1>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo</span>
+                            </label>
+                            <input type="url" name='url' placeholder="photo_url" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                <Link className="label-text-alt link link-hover">Forgot password?</Link>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn border-none bg-cyan-700 ">Register</button>
+                        </div>
+                        <div className="flex p-[1px] justify-between">
+                            <span>All ready have an account</span>
+                            <span className='hover:text-red-400'> <Link to='/login'>Please login</Link></span>
+                        </div>
+                    </from>
+                </div>
+            </div>
         </div>
     );
 };
